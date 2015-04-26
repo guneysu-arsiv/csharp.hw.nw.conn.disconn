b6f37bb4ac3b2a63ca6f208a43fec783d5d87033
Ahmed Şeref <serefguneysu@gmail.com>
2015-04-26 13:40:44 +0300

    Products (Disconnected) mimari completed

--- AdoConnectedDisconnected/ui/Products.Designer.cs
+++ AdoConnectedDisconnected/ui/Products.Designer.cs
@@ -28,21 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.lstUnitPrices = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.ItemHeight = 17;
+            this.lstProducts.Location = new System.Drawing.Point(17, 43);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(156, 293);
+            this.lstProducts.TabIndex = 0;
+            // 
+            // lstUnitPrices
+            // 
+            this.lstUnitPrices.FormattingEnabled = true;
+            this.lstUnitPrices.ItemHeight = 17;
+            this.lstUnitPrices.Location = new System.Drawing.Point(179, 43);
+            this.lstUnitPrices.Name = "lstUnitPrices";
+            this.lstUnitPrices.Size = new System.Drawing.Size(146, 293);
+            this.lstUnitPrices.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürünler";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(179, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Birim Fiyatlar";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Products
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 349);
+            this.ClientSize = new System.Drawing.Size(356, 349);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstUnitPrices);
+            this.Controls.Add(this.lstProducts);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Name = "Products";
+            this.Padding = new System.Windows.Forms.Padding(17, 40, 17, 19);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Products";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Products_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.ListBox lstUnitPrices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AdoConnectedDisconnected/ui/Products.cs
+++ AdoConnectedDisconnected/ui/Products.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,42 @@ namespace AdoConnectedDisconnected.ui
         public Products()
         {
             InitializeComponent();
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter;
+            DataTable dt = new DataTable("Products");
+            dt.Columns.Add("ProductName", typeof(string));
+            dt.Columns.Add("UnitPrice", typeof(float));
+
+            //dt.Rows.Add("Baklava", 30.0);
+            //dt.Rows.Add("Baklava", 30.0);
+            //dt.Rows.Add("Baklava", 30.0);
+            //dt.Rows.Add("Baklava", 30.0);
+            //dt.Rows.Add("Baklava", 30.0);
+
+            try
+            {
+                adapter = new SqlDataAdapter(
+                   selectCommandText: "select ProductName, UnitPrice from Products",
+                   selectConnectionString: "server=.;database=northwnd;integrated security=sspi;");
+            }
+            catch (Exception)
+            {
+                adapter = new SqlDataAdapter(
+                   selectCommandText: "select ProductName, UnitPrice from Products",
+                   selectConnectionString: "server=.;database=northwind;integrated security=sspi;");
+            }
+
+            adapter.Fill(dataTable: dt);
+
+            lstProducts.DataSource = dt;
+            lstUnitPrices.DataSource = dt;
+
+            lstProducts.DisplayMember = "ProductName";
+            lstUnitPrices.DisplayMember = "UnitPrice";
+
         }
     }
 }
